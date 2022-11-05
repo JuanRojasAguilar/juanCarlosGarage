563614f236c6ad21dd07fcb68374d8484bd246e9
Juan <yo217@hotmail.es>
2022-11-05 18:49:50 -0500

    presentation added

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,6 @@
 import React from "react"
 import Header from "@components/Header"
+import Presentation from "@components/Presentation"
 
 import "@styles/Home.scss"
 
@@ -7,6 +8,7 @@ const Home = () => {
     return(
         <>
             <Header />
+            <Presentation />
         </>
     )
 }
