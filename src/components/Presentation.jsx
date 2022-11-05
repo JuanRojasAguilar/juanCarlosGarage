019cd362d8c231f517a3bdf38a3776de4e95ac23
Juan <yo217@hotmail.es>
2022-11-05 18:54:17 -0500

    assets added

---
+++ src/components/Presentation.jsx
@@ -0,0 +1,14 @@
+import React from "react"
+import "@styles/Presentation.scss"
+
+const Presentation = () => {
+    return(
+        <div className="presentationDiv">
+            <p>ARTISTAS DE LA ARTESANÍA AUTOMOTRIZ</p>
+            <h1>AUTOS INCREIBLES<br />HECHOS REALIDAD</h1>
+            <button>CONOCE EL TALLER</button>
+        </div>
+    )
+}
+
+export default Presentation
